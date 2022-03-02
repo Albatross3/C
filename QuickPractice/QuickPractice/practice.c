20d9a57468df36362b7ba97cf9373af01cd121e4
DongJunLee <superstring77@gmail.com>
2022-03-03 02:49:22 +0900

    week2

---
+++ QuickPractice/QuickPractice/practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) 
+{
+	int i = 1, j = 2;
+
+	printf("%d\n",i*j/i);
+	printf("%d\n",i);
+	printf("%d\n",j);
+
+	return 0;
+}
--- QuickPractice/QuickPractice/pun.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) 
-{
-	int i = 10 % -3;
-	printf("%d\n", i);
-	printf("I love you");
-	return 0;
-}
