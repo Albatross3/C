4033e2465215eee124e00c527f97205b99dd73b3
DongJunLee <superstring77@gmail.com>
2022-03-21 11:23:25 +0900

    week5 practice

--- QuickPractice/QuickPractice/practice.c
+++ QuickPractice/QuickPractice/practice.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 int main(void) 
 {
-	int area_code;
-	printf("Enter Area Code: ");
-	scanf_s("%d", &area_code);
-	switch (area_code) {
-	case 229: printf("Albany");
-		break;
-	case 404: case 470: case 678: case 770: printf("Atlanta");
-		break;
-	case 478: printf("Macon");
-		break;
-	case 706: case 762: printf("Columbus");
-		break;
-	case 912: printf("Savannah");
-		break;
-	default: printf("Area code not recognized");
-		break;
-	}
+	int len = 0;
 
+	printf("Enter a message: ");
+	while (getchar() != '\n')
+		len++;
+	printf("Your message was %d character(s) long.\n", len);
 
 	return 0;
 }
