b6bf1cec0ce6ed37f037b720f7f7d714f9581e4c
DongJunLee <superstring77@gmail.com>
2022-06-01 23:35:24 +0900

    6/1

--- QuickPractice/QuickPractice/QuickPractice.c
+++ QuickPractice/QuickPractice/QuickPractice.c
@@ -1,32 +1,10 @@
 #include <stdio.h>
-void swap(int* p, int* q);
+#include <string.h>
 int main(void)
 {
-	int a = 1,b = 3;
-	printf("%d, %d", a, b);
-	swap(&a, &b);
-	printf("%d, %d", a, b);
-
+	char* s1 = "abc";
+	char* s2 = "abcd";
+	int a = strcmp(4, s2);
+	printf("%d", a);
 	
-}
-void avg_sum(double a[], int n, double* avg, double* sum) {
-	int i;
-
-	*sum = 0.0;
-	for (i = 0; i < n; i++) 
-		*sum += a[i];
-	*avg = *sum / n;
-}
-void swap(int* p, int* q) {
-	int temp = *p;
-	*p = *q;
-	*q = temp;
-}
-int sum_array(const int a[], int n) {
-	int* p, sum;
-	sum = 0;
-	for (p = a; p< a + n; p++) {
-		sum += *p;
-	}
-	return sum;
 }
